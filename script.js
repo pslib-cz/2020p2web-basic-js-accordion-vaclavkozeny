349c61947a32278cbb834bbc4778f9aa42ef2d3e
Václav Kožený <u19.413@pslib.cloud>
2021-03-07 12:16:42 +0100

    funguje bez designu

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let p = document.getElementsByClassName("vypujcka-item")
+console.log(p)
 function menu(){
-    console.log(1)
+    for(let i = 0; i <p.length;i++){
+   p[i].getElementsByTagName("input")[0].onclick = (e) => {p[i].getElementsByClassName("detail-vypujcky-container-layout")[0].classList.toggle("detail-vypujcky-container")}}
+   
 }
+
+ 
