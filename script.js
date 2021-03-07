7759493a8cc9279e389c027c3c22fa598f4e0fa9
Václav Kožený <u19.413@pslib.cloud>
2021-03-07 14:52:29 +0100

    hotovo

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let p = document.getElementsByClassName("vypujcka-item")
-console.log(p)
+
 function menu(){
     for(let i = 0; i <p.length;i++){
-   p[i].getElementsByTagName("input")[0].onclick = (e) => {p[i].getElementsByClassName("detail-vypujcky-container-layout")[0].classList.toggle("detail-vypujcky-container")}}
-   
-}
+   p[i].getElementsByTagName("input")[0].onclick = () => {p[i].getElementsByClassName("detail-vypujcky-container-layout")[0].classList.toggle("detail-vypujcky-container")
+    if(p[i].getElementsByClassName("detail-vypujcky-container-layout")[0].classList.contains("detail-vypujcky-container")){
+        p[i].style.height = "32px"
+    }
+    else{
+        p[i].style.height = p[i].scrollHeight + "px"
+    }}}}
 
+window.onload = menu
  
